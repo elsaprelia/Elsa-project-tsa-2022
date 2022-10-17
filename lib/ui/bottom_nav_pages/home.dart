0dde68d70f1848026aee1fe8a0e21d78d8ad781e
elsaprelia <elsaaprelianisutrisno@gmail.com>
2022-10-17 22:39:53 +0700

    6

--- lib/ui/bottom_nav_pages/home.dart
+++ lib/ui/bottom_nav_pages/home.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dots_indicator/dots_indicator.dart';
--- lib/ui/user_form.dart
+++ lib/ui/user_form.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/const/AppColors.dart';
 import 'package:flutter_ecommerce/ui/bottom_nav_controller.dart';
