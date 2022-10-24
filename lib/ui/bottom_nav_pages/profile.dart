55f2877fe12e126977409fd94d2714034779830d
elsaprelia <elsaaprelianisutrisno@gmail.com>
2022-10-24 07:44:14 +0700

    embo kah

--- lib/ui/bottom_nav_pages/profile.dart
+++ lib/ui/bottom_nav_pages/profile.dart
@@ -8,55 +8,59 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  TextEditingController ?_nameController;
-  TextEditingController ?_phoneController;
-  TextEditingController ?_ageController;
+  TextEditingController? _nameController;
+  TextEditingController? _phoneController;
+  TextEditingController? _ageController;
 
-
-  setDataToTextField(data){
-    return  Column(
+  setDataToTextField(data) {
+    return Column(
       children: [
         TextFormField(
-          controller: _nameController = TextEditingController(text: data['name']),
+          controller: _nameController =
+              TextEditingController(text: data['name']),
         ),
         TextFormField(
-          controller: _phoneController = TextEditingController(text: data['phone']),
+          controller: _phoneController =
+              TextEditingController(text: data['phone']),
         ),
         TextFormField(
           controller: _ageController = TextEditingController(text: data['age']),
         ),
-        ElevatedButton(onPressed: ()=>updateData(), child: Text("Update"))
+        ElevatedButton(onPressed: () => updateData(), child: Text("Update"))
       ],
     );
   }
 
-  updateData(){
-    CollectionReference _collectionRef = FirebaseFirestore.instance.collection("users-form-data");
-    return _collectionRef.doc(FirebaseAuth.instance.currentUser!.email).update(
-        {
-          "name":_nameController!.text,
-          "phone":_phoneController!.text,
-          "age":_ageController!.text,
-        }
-        ).then((value) => print("Updated Successfully"));
+  updateData() {
+    CollectionReference _collectionRef =
+        FirebaseFirestore.instance.collection("users-form-data");
+    return _collectionRef.doc(FirebaseAuth.instance.currentUser!.email).update({
+      "name": _nameController!.text,
+      "phone": _phoneController!.text,
+      "age": _ageController!.text,
+    }).then((value) => print("Updated Successfully"));
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Padding(
+      body: SafeArea(
+          child: Padding(
         padding: const EdgeInsets.all(20.0),
         child: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection("users-form-data").doc(FirebaseAuth.instance.currentUser!.email).snapshots(),
-          builder: (BuildContext context, AsyncSnapshot snapshot){
+          stream: FirebaseFirestore.instance
+              .collection("users-form-data")
+              .doc(FirebaseAuth.instance.currentUser!.email)
+              .snapshots(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
             var data = snapshot.data;
-            if(data==null){
-              return Center(child: CircularProgressIndicator(),);
+            if (data == null) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
             }
             return setDataToTextField(data);
           },
-
         ),
       )),
     );
