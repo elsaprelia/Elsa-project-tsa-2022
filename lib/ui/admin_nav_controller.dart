d80729149548e001e0df3952e4be58fc584128b6
elsaprelia <elsaaprelianisutrisno@gmail.com>
2022-11-03 10:44:22 +0700

    qq

---
+++ lib/ui/admin_nav_controller.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/const/AppColors.dart';
+import 'package:flutter_ecommerce/ui/bottom_nav_pages/home.dart';
+
+class AdminNavController extends StatefulWidget {
+  @override
+  _AdminNavControllerState createState() => _AdminNavControllerState();
+}
+
+class _AdminNavControllerState extends State<AdminNavController> {
+  final _pages = [
+    Home(),
+  ];
+  var _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "Mulyorejo Gold",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 5,
+        selectedItemColor: AppColors.deep_orange,
+        backgroundColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _currentIndex,
+        selectedLabelStyle:
+            TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        items: [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(Icons.home),
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            print(_currentIndex);
+          });
+        },
+      ),
+      body: _pages[_currentIndex],
+    );
+  }
+}
