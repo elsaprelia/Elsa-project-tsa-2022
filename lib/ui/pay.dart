bb227943cdd5ad40fcb388d668938ab53f334c9e
elsaprelia <elsaaprelianisutrisno@gmail.com>
2022-11-12 07:17:11 +0700

    HHHH

---
+++ lib/ui/pay.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/widgets/fetchProducts.dart';
+
+class Pay extends StatefulWidget {
+  Pay(product);
+
+  @override
+  _PayState createState() => _PayState();
+}
+
+class _PayState extends State<Pay> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
