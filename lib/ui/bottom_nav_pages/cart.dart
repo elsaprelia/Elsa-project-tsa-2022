984af4b1fa0a61ce5ce372d661bb79b40cfcc817
elsaprelia <elsaaprelianisutrisno@gmail.com>
2022-11-15 14:11:34 +0700

    payment

--- lib/ui/bottom_nav_pages/cart.dart
+++ lib/ui/bottom_nav_pages/cart.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/ui/bottom_nav_pages/favourite.dart';
+import 'package:flutter_ecommerce/ui/pay.dart';
 import 'package:flutter_ecommerce/widgets/fetchProducts.dart';
 
 class Cart extends StatefulWidget {
@@ -11,8 +14,11 @@ class _CartState extends State<Cart> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+          child: InkWell(
+        onTap: () =>
+            Navigator.push(context, CupertinoPageRoute(builder: (_) => Pay())),
         child: fetchData("users-cart-items"),
-      ),
+      )),
     );
   }
 }
--- lib/ui/pay.dart
+++ lib/ui/pay.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/widgets/fetchProducts.dart';
 
 class Pay extends StatefulWidget {
-  Pay(product);
-
   @override
   _PayState createState() => _PayState();
 }
@@ -11,6 +9,10 @@ class Pay extends StatefulWidget {
 class _PayState extends State<Pay> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: fetchData("users-favourite-items"),
+      ),
+    );
   }
 }
