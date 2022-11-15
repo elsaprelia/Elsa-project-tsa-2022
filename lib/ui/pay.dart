4ac62054adbae232f9f7eb4e9fedca3ee0d14460
elsaprelia <elsaaprelianisutrisno@gmail.com>
2022-11-15 14:49:24 +0700

    finis

--- lib/ui/pay.dart
+++ lib/ui/pay.dart
@@ -10,8 +10,22 @@ class _PayState extends State<Pay> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: fetchData("users-favourite-items"),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "PEMBAYARAN",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: Text("LAKUKAN PEMBAYARAN VIA TRANSFER KE REKENING: "
+            "BCA : 0875987698 "
+            "A/N ELSA APRELIA "
+            "KONFIRMASI KE ADMIN DENGAN WA KE NOMOR 085646485283 "
+            "SERTAKAN BUKTI TRANSFER DAN ALAMAT PENGIRIMAN ATAU DIAMBIL KE OFFLINE STORE"),
       ),
     );
   }
